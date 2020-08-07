b9061ff347c06985af52221291e1c29d93627b3e
Aztec Rabbit <ars.xda@gmail.com>
2020-08-07 14:26:06 +0700

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
 	name='multithreading',
-	version='0.0.1',
+	version='0.0.2',
 	author='aztecrabbit',
 	author_email='ars.xda@gmail.com',
 	description='MultiThreading: Thread + Queue',
