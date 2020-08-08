b87fcc726d628e2f6a4a48f3a7df6b2d59443106
Aztec Rabbit <ars.xda@gmail.com>
2020-08-08 13:35:47 +0700

    Add default var and get method of special chars

--- multithreading/logger.py
+++ multithreading/logger.py
@@ -4,28 +4,68 @@ from threading import RLock
 
 from loguru import logger
 
+special_chars_code = {
+	# Disabled
+	'D': '30',
+	# Red
+	'R': '31',
+	# Green
+	'G': '32',
+	# Yellow
+	'Y': '33',
+	# Blue
+	'B': '34',
+	# Purple
+	'P': '35',
+	# Cyan
+	'C': '36',
+	# White
+	'W': '37',
+}
+
+special_chars = {
+	# Clear Color
+	'CC': '\033[0m',
+	# Clear Line
+	'CN': '\033[2K',
+	# To First Line
+	'CR': '\r',
+}
+
+
+def get_special_char(key=None, char_type=1):
+	"""
+	char_type
+	```
+	0 = normal
+	1 = bold (light)
+	2 = dark
+	3 = italic
+	4 = underline
+	5 = blink
+	6 = blink again (?)
+	7 = reverse
+	```
+	"""
+	if (special_char := special_chars.get(key)) is not None:
+		return special_char
+
+	if key is not None and (char_code := special_chars_code.get(key)) is not None:
+		return f'\033[{char_code};{char_type}m'
+
+	return f'\033[{char_type}m'
+
+
+for code in special_chars_code:
+	for i in range(1, 9 + 1):
+		special_chars[f'{code}{i}'] = get_special_char(code, i)
+
 
 class Logger:
 	def __init__(self, level='DEBUG'):
 		self._lock = RLock()
 
-		self.special_chars = {
-			'R1': '\033[31;1m',
-			'R2': '\033[31;2m',
-			'G1': '\033[32;1m',
-			'G2': '\033[32;2m',
-			'Y1': '\033[33;1m',
-			'Y2': '\033[33;2m',
-			'B1': '\033[34;1m',
-			'B2': '\033[34;2m',
-			'P1': '\033[35;1m',
-			'P2': '\033[35;2m',
-			'C1': '\033[36;1m',
-			'C2': '\033[36;2m',
-			'CC': '\033[0m',
-			'CN': '\033[2K',
-			'CR': '\r',
-		}
+		self.special_chars = special_chars
 
 		self.logger = logger
 		self.logger.remove()
