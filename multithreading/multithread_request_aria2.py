81ba72e4fc01f576c1a84cf8108f5af46f53eeca
Aztec Rabbit <ars.xda@gmail.com>
2020-10-13 01:11:02 +0700

    Add more features
    1. Add method request_rpc
    2. Add method aria2_get_stopped_list
    3. Add method aria2_remove_download_result
    4. Add method aria2_clear_completed_list
    5. Better rpc result error handling

--- multithreading/multithread_request_aria2.py
+++ multithreading/multithread_request_aria2.py
@@ -5,23 +5,13 @@ class MultiThreadRequestAria2(MultiThreadRequest):
 	aria2_rpc = 'http://127.0.0.1:6800/jsonrpc'
 	aria2_rpc_secret = ''
 
-	def download(self, url, dirname='', filename=''):
-		options = {}
-
-		if dirname:
-			options['dir'] = dirname
-
-		if filename:
-			options['out'] = filename
-
-		content = {
+	def request_rpc(self, data):
+		default_data = {
 			'jsonrpc': '2.0',
 			'id': 'aztecrabbit:multithreading',
-			'method': 'aria2.addUri',
-			'params': [f'token:{self.aria2_rpc_secret}', [url], options],
 		}
 
-		response = self.request('post', self.aria2_rpc, json=content)
+		response = super().request('post', self.aria2_rpc, json=self.dict_merge(default_data, data))
 
 		R1 = self.logger.special_chars['R1']
 		CC = self.logger.special_chars['CC']
@@ -31,11 +21,60 @@ class MultiThreadRequestAria2(MultiThreadRequest):
 		if data.get('error'):
 			self.log('\n'.join([
 				f"{R1}Response Error{CC}",
-				f"  {data.get('message')}",
+				f"  {data['error'].get('message')}",
 				f"",
 			]))
 
-			return
+			self.task_complete()
+
+			raise ValueError
+
+		return response
+
+	def aria2_get_stopped_list(self):
+		response = self.request_rpc({
+			'method': 'aria2.tellStopped',
+			'params': [f'token:{self.aria2_rpc_secret}', 0, 10000],
+		})
+
+		data = response.json()
+
+		return data['result']
+
+	def aria2_remove_download_result(self, gid):
+		response = self.request_rpc({
+			'method': 'aria2.removeDownloadResult',
+			'params': [f'token:{self.aria2_rpc_secret}', gid],
+		})
+
+		R1 = self.logger.special_chars['R1']
+
+		data = response.json()
+
+		if data['result'] == 'OK':
+			self.log(f'{R1}{gid} - Removed')
+
+		else:
+			self.log(f'{data}')
+
+	def aria2_clear_completed_list(self):
+		for data in self.aria2_get_stopped_list():
+			if data['status'] == 'complete':
+				self.aria2_remove_download_result(data['gid'])
+
+	def download(self, url, dirname='', filename=''):
+		options = {}
+
+		if dirname:
+			options['dir'] = dirname
+
+		if filename:
+			options['out'] = filename
+
+		response = self.request_rpc({
+			'method': 'aria2.addUri',
+			'params': [f'token:{self.aria2_rpc_secret}', [url], options],
+		})
 
 		self.download_added(response, url, dirname, filename)
 
